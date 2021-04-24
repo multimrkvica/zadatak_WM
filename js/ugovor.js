191e08738335e885900e383ff4053058492fd62b
multimrkvica <mrkvica.macak@gmail.com>
2021-04-24 20:45:26 +0200

    dropdown

--- js/ugovor.js
+++ js/ugovor.js
@@ -1,31 +1,57 @@
+ // mySelect.textContent = jsonObj['promo_text']; //json objekt ovo radi vadi sta treba kad vadim samo objkat u suprotnom moram da pretvorim u string i parsiram kao u primeru sa obj_pars.
+
+
+
 /// html elementi u varijablama
 var dropdown = document.querySelector('div.box_list');
+var box_1 = document.querySelector('div.cont_sec_1');
+var box_2 = document.querySelector('div.cont_sec_2');
+var box_3 = document.querySelector('div.cont_sec_3');
 
 // get json 
 var requestURL = "http://www.mocky.io/v2/5ed511c53300005f00f7a790";
+
 var request = new XMLHttpRequest();
-var contract;
+
 request.open("GET", requestURL);
 request.responseType = "json";
 request.send();
 
+var contract;
+
 request.onload = function () {
     contract = request.response;
+
     //povezujem funkcije da hvataju response
-    populateDropdown(contract);
+    populateDropdown(contract); 
+    populatebox_1(contract);
+   
+
 }
 ///DROPDOWN
-function populateDropdown(jsonObj) {
-    let mySelect = document.createElement('div') //pravi element u html-u
-    mySelect.classList.add('first_drop')//dodaje klasu
+function populateDropdown() {
+
+    let mySelect = document.createElement('select') //pravi element u html-u
+    mySelect.classList.add('drop_wrep')//dodaje klasu
 
     //vadim niz
-    let obj_string = JSON.stringify(contract.contract_length.preselected_contract_length);
+    let constact_list_string = JSON.stringify(contract.contract_length.contract_length_options);
     // parsiranje
-    let obj_pars = JSON.parse(obj_string);
+    let constact_list_pars = JSON.parse(constact_list_string);
 
-    mySelect.textContent = obj_pars;
+    let constact_list = constact_list_pars.reverse();
+
+     for ( var i = 0; i < constact_list.length; i++){
+        let myOption = document.createElement('option');
+        myOption.classList.add('option_drop');
+        let txt = document.createTextNode(constact_list[i]);
+        mySelect.appendChild(myOption);
+        myOption.appendChild(txt);
+        myOption.setAttribute ("value", constact_list[i])
+        console.log(txt);
+     }
     dropdown.appendChild(mySelect);
-    console.log(dropdown)
-}
 
+    console.log(dropdown);
+    console.log(constact_list);
+}
